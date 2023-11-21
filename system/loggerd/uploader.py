230314e581d075eae4fce74393ecdb06cf4354b8
mahlzeit1948 <32252711+mahlzeit1948@users.noreply.github.com>
2023-11-21 18:41:55 -0500

    upload to NAS

--- system/loggerd/uploader.py
+++ system/loggerd/uploader.py
@@ -11,6 +11,10 @@ import traceback
 from pathlib import Path
 from typing import BinaryIO, Iterator, List, Optional, Tuple, Union
 
+# begin mahlzeit .
+from ftplib import FTP
+# end mahlzeit
+
 from cereal import log
 import cereal.messaging as messaging
 from openpilot.common.api import Api
@@ -29,6 +33,7 @@ UPLOAD_QLOG_QCAM_MAX_SIZE = 100 * 1e6  # MB
 
 allow_sleep = bool(os.getenv("UPLOADER_SLEEP", "1"))
 force_wifi = os.getenv("FORCEWIFI") is not None
+force_wifi = True #mahlzeit temp
 fake_upload = os.getenv("FAKEUPLOAD") is not None
 
 OFFROAD_TRANSITION_TIMEOUT = 900.  # wait until offroad for 15 minutes before allowing uploads
@@ -140,6 +145,13 @@ class Uploader:
       if any(f in fn for f in self.immediate_folders):
         return name, key, fn
 
+    #begin mahlzeit
+    for name, key, fn in upload_files:
+      if name[-4:] == "hevc":
+        return (name, key, fn)
+    #end mahlzeit  
+
+    
     for name, key, fn in upload_files:
       if name in self.immediate_priority:
         return name, key, fn
@@ -180,12 +192,74 @@ class Uploader:
     self.last_exc = None
 
     try:
-      self.do_upload(key, fn)
+      #self.do_upload(key, fn)
+      self.do_upload_mahlzeit(key, fn)
     except Exception:
       pass
 
     return self.last_resp
 
+# begin mahlzeit
+  def do_upload_mahlzeit(self, key, fn):
+    #print('mahlzeit upload')
+    if key[-4:] != 'hevc':
+      #print('extension is not hevc')
+      #print(key)
+      #self.status_code = 200
+      class FakeResponse():
+        def __init__(self):
+          self.status_code = 200
+          self.request = FakeRequest()
+      self.last_resp = FakeResponse()
+      return
+
+    try:
+      #print('connecting to ftp')
+      ftp = FTP(host = '192.168.0.202', user = 'comma2', passwd = 'comma2')
+      ftp.cwd('/dashcam')
+      #check if folder exists
+      folder_tmp = (key[:key.rfind('/')])
+      folder = folder_tmp[0:20] #only one folder per drive
+      #print('folder')
+      print (folder)
+      file = key[key.rfind('/')+1:]
+      file = str(folder_tmp[folder_tmp.rfind('-')+1:]).zfill(4) + '-' + file  #pad with leading zeroes
+      #print('file')
+      #print(file)
+      if not folder in ftp.nlst():
+        #create folder
+        ftp.mkd(folder)
+
+      #switch to folder
+      ftp.cwd(folder)
+
+      #upload file
+      ftpcommand = "STOR %s"%file;
+      ftp.storbinary(ftpcommand, open(fn, 'rb'))
+      class FakeResponse():
+        def __init__(self):
+          self.status_code = 201
+          self.request = FakeRequest()
+      self.last_resp = FakeResponse()
+      return
+
+    except ftplib.error_perm as e:
+      #print('error during ftp')
+      class FakeResponse():
+        def __init__(self):
+          self.status_code = 404
+          self.request = FakeRequest()
+      self.last_resp = FakeResponse()
+      return      
+      #self.last_resp.status_code = 404
+      self.last_exc = 'dummy'
+      raise
+
+    finally:
+      ftp.quit()
+
+# end mahlzeit     
+  
   def upload(self, name: str, key: str, fn: str, network_type: int, metered: bool) -> bool:
     try:
       sz = os.path.getsize(fn)
